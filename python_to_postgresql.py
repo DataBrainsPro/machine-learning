7c2fad76d993a406b8fee0c2c7c95cd728da4239
DataBrains Pro <databrainspro@gmail.com>
2022-11-17 07:40:56 +0530

    connection to db established

--- python_to_postgresql.py
+++ python_to_postgresql.py
@@ -1,12 +1,37 @@
-# need to add postgresql connection attributes
-
 import psycopg2
 
-conn = psycopg2.connect()
+hostname = 'localhost'
+database = 'DataBrains'
+username = 'postgres'
+pwd = 'MLN@sql@7'
+port_id = 5432
+
+conn = None
+cur = None
 
 try:
+    conn = psycopg2.connect(
+                host = hostname,
+                dbname = database,
+                user = username,
+                password = pwd,
+                port = port_id)
+    
+    cur = conn.cursor()
 
-except:
+    create_script = '''CREATE TABLE IF NOT EXISTS employee (
+                            id      int PRIMARY KEY,
+                            name    varchar(40) NOT NULL,
+                            salary  int,
+                            dept_id varchar(30))'''
 
+    cur.execute(create_script)
+   
+except Exception as error:
+    print(error)
 finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
 
